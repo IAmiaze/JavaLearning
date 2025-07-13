d7d4373ba607858987b2b97cfec18e816432da36
IAmiaze <ayub@erainfotechbd.com>
2025-07-13 18:44:03 +0600

    Refactor getGrade method to use switch-case for improved readability

--- Method.java
+++ Method.java
@@ -5,36 +5,42 @@ public class Method {
     }
 
     public static String getGrade(double gpa) {
-        if (gpa >= 4.0) {
-            return "A+";
-        } else if (gpa >= 3.7) {
-            return "A";
-        } else if (gpa >= 3.3) {
-            return "A-";
-        } else if (gpa >= 3.0) {
-            return "B+";
-        } else if (gpa >= 2.7) {
-            return "B";
-        } else if (gpa >= 2.3) {
-            return "B-";
-        } else if (gpa >= 2.0) {
-            return "C+";
-        } else if (gpa >= 1.7) {
-            return "C";
-        } else if (gpa >= 1.0) {
-            return "D";
-        } else {
-            return "F";
+        int gradeCase = (int) (gpa * 10);
+        switch (gradeCase) {
+            case 40: // 4.0 and above
+            case 41: case 42: case 43: case 44: case 45: case 46: case 47: case 48: case 49: case 50:
+                return "A+";
+            case 37: case 38: case 39:
+                return "A";
+            case 33: case 34: case 35: case 36:
+                return "A-";
+            case 30: case 31: case 32:
+                return "B+";
+            case 27: case 28: case 29:
+                return "B";
+            case 23: case 24: case 25: case 26:
+                return "B-";
+            case 20: case 21: case 22:
+                return "C+";
+            case 17: case 18: case 19:
+                return "C";
+            case 10: case 11: case 12: case 13: case 14: case 15: case 16:
+                return "D";
+            default:
+                return "F";
         }
     }
 
     // Example usage: print student grade
+    
     public static void printStudentGrade(double gpa) {
         String grade = getGrade(gpa);
         System.out.println("Student GPA: " + gpa + ", Grade: " + grade);
     }
 
     public static void main(String[] args) {
+        double gpa = 4.5;
+        printStudentGrade(gpa);
         double amount = 1000.0;
         double vatRate = 15.0; // 15% VAT
         double vat = calculateVAT(amount, vatRate);
